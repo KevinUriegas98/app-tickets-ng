ffa3065ee99760dc650d6452be41254fdd9f3189
KevinUriegas98 <102544692+KevinUriegas98@users.noreply.github.com>
2024-08-20 16:19:53 -0600

    [Update] Login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
@@ -8,7 +9,7 @@ import { faHeart, faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 import { images } from '@Constants';
 // Models //
-import { LoginRequest } from '@Models/Auth'
+import { LoginRequest } from '@Models/Auth';
 // Services //
 import { LoginService } from '@Services';
 
@@ -19,61 +20,86 @@ import { LoginService } from '@Services';
   standalone: true,
   imports: [ReactiveFormsModule, FontAwesomeModule, NgIf]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   readonly images = images;
-
   private fb = inject(FormBuilder);
   private auth = inject(LoginService);
   private router = inject(Router);
   private toastr = inject(ToastrService);
-  year:number = 0;
+  year: number = 0;
+  private isBrowser: boolean;
 
-  constructor(library: FaIconLibrary) {
+  constructor(library: FaIconLibrary, @Inject(PLATFORM_ID) platformId: any) {
     library.addIcons(
       faHeart,
       faUser,
       faLock
     );
+    this.isBrowser = isPlatformBrowser(platformId);
   }
   
   form = this.fb.nonNullable.group({
-    usuario: ['', [Validators.required]],
-    password: ['', [Validators.required]]
+    usuario: ['kevin@correo.com', [Validators.required]],
+    password: ['12345', [Validators.required]]
   });
 
+  toggleDarkMode(): void {
+    if (this.isBrowser) {
+      const body = document.body;
+      const isDarkMode = body.classList.contains('dark');
+      console.log("isDarkMode", isDarkMode);
+      body.classList.toggle('dark', !isDarkMode);
+      localStorage.setItem('theme', !isDarkMode ? 'dark' : 'light');
+    }
+  }
+
   ngOnInit(): void {
-    var  d = new  Date();
+    const d = new Date();
     this.year = d.getFullYear();
+
+    if (this.isBrowser) {
+      const themeToggleButton = document.getElementById('theme-toggle');
+      if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', () => this.toggleDarkMode());
+      }
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme === 'dark') {
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
+    }
   }
 
   onSubmit(): void {
     if (this.form.valid) {
-      const { usuario, password } = this.form.getRawValue();
-      const request: LoginRequest = {
-        username: usuario,
-        userpassword: password
-      };
-      this.auth.auth(request)
-        .subscribe({
-          next: (res) => {
-            const data = res.Response.data;
-            localStorage.setItem('token', data.Token);
-            localStorage.setItem('idUsuario', data.Usuario.Id.toString());
-            localStorage.setItem('idPerfil', data.Usuario.IdPerfil.toString());
-            localStorage.setItem('usuario', data.Usuario.NombreUsuario);
-            localStorage.setItem('nombrePersona', data.Usuario.NombrePersona);
-            localStorage.setItem('idSucursal', data.Usuario.IdSucursal.toString());
-            localStorage.setItem('sucursal', data.Usuario.NombreSucursal);
-            localStorage.setItem('pctDescuento', data.Usuario.PctDescuento.toString());
-            if(!localStorage.getItem('mode')){
-              localStorage.setItem('mode', 'light');
-            }
-            this.router.navigate(['/home']);
-          },
-          error: (err) => {
-            this.toastr.error('Ha Ocurrido un Error', err);
-          }
-        });
+      this.router.navigate(['/home']);
+      // const { usuario, password } = this.form.getRawValue();
+      // const request: LoginRequest = {
+      //   username: usuario,
+      //   userpassword: password
+      // };
+      // this.auth.auth(request)
+      //   .subscribe({
+      //     next: (res) => {
+      //       const data = res.Response.data;
+      //       localStorage.setItem('token', data.Token);
+      //       localStorage.setItem('idUsuario', data.Usuario.Id.toString());
+      //       localStorage.setItem('idPerfil', data.Usuario.IdPerfil.toString());
+      //       localStorage.setItem('usuario', data.Usuario.NombreUsuario);
+      //       localStorage.setItem('nombrePersona', data.Usuario.NombrePersona);
+      //       localStorage.setItem('idSucursal', data.Usuario.IdSucursal.toString());
+      //       localStorage.setItem('sucursal', data.Usuario.NombreSucursal);
+      //       localStorage.setItem('pctDescuento', data.Usuario.PctDescuento.toString());
+      //       if (!localStorage.getItem('mode')) {
+      //         localStorage.setItem('mode', 'light');
+      //       }
+      //       this.router.navigate(['/home']);
+      //     },
+      //     error: (err) => {
+      //       this.toastr.error('Ha Ocurrido un Error', err);
+      //     }
+      //   });
     } else {
       this.form.markAllAsTouched();
     }
