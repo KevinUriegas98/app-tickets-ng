f2b94b84e550097a176665765bb1b137741ca23b
KevinUriegas98 <102544692+KevinUriegas98@users.noreply.github.com>
2024-08-20 16:22:01 -0600

    [Add] Logo App

--- src/app/global/constants.ts
+++ src/app/global/constants.ts
@@ -1,7 +1,10 @@
 export const images = {
   notfound: '../../assets/dist/image/not-found.png',
   logo: '../../assets/dist/image/logo.svg',
+  logoNg: '../../assets/dist/image/logoNg.png',
   logoDos: '../../assets/dist/image/logo2.svg',
   logoTres: '../../assets/dist/image/logo3.jpg',
   logoCuatro: '../../assets/dist/image/logo4.png',
 }
+
+export const nameApp = 'Help Desk | '
