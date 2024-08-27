12dce72608b8281697294ab7c2071c330fada4c4
KevinUriegas98 <102544692+KevinUriegas98@users.noreply.github.com>
2024-08-26 21:11:48 -0600

    [Add] AlertService

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { routes } from './app.routes';
@@ -10,11 +11,14 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(ToastrModule.forRoot({
-      timeOut: 5000,
-      positionClass: 'toast-top-right',
-      preventDuplicates: true,
-    })),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        timeOut: 5000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+      }),
+      SweetAlert2Module.forRoot()
+    ),
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations()
