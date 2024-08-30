ae8507b5d6f191ce7a912a3998b08e1d8f1d7bb1
Debban-art <debbany.545@gmail.com>
2024-08-30 13:10:51 -0600

    Tabla Modulos

--- src/app/modules/administracion/pages/modulos/modulos.component.ts
+++ src/app/modules/administracion/pages/modulos/modulos.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { CustomTableComponent } from '@Component/Table';
+import { SweetAlertService } from '@Service/SweetAlert';
+
+import { ModuloModel } from '@Models/Modulo';
+import { ModuloService } from '@Services';
 
 @Component({
   selector: 'app-modulos',
   standalone: true,
-  imports: [],
+  imports: [CustomTableComponent],
   templateUrl: './modulos.component.html',
   styleUrl: './modulos.component.css'
 })
 export class ModulosComponent {
+  constructor() { }
+  private moduloService = inject(ModuloService);
+  private sweetAlertService = inject(SweetAlertService);
 
+  modulosList: ModuloModel[] = [];
+
+  ngOnInit(): void {
+    this.getAllModulos();
+  }
+
+  getAllModulos() {
+    this.moduloService.getAllModulos().subscribe((data) => {
+      this.modulosList = data.response;
+    });
+  }
+
+  editModulo(data: ModuloModel) {
+    console.log(data);
+  }
+
+  deleteModulo(Modulo_Id: number) {
+    console.log(Modulo_Id)
+  }
 }
