76a4f715c26a5fbb54902ceba092086630ce6180
Debban-art <debbany.545@gmail.com>
2024-10-07 20:15:20 -0600

    Formulario Usuario-Sistema

--- src/app/modules/administracion/pages/usuario-sistema/usuario-sistema.component.ts
+++ src/app/modules/administracion/pages/usuario-sistema/usuario-sistema.component.ts
@@ -39,11 +39,66 @@ export class UsuarioSistemaComponent {
 
   ngOnInit(): void{
     this.getAllUsuariosSistemas();
+    this.getAllSistemas();
+    this.getAllUsuarios();
   }
 
   getAllUsuariosSistemas() {
     this.usuarioSistemaService.getAllUsuariosSistemas().subscribe((data) => {
       this.usuarioSistemaList = data.response;
+    })
+  }
+
+  getAllSistemas() {
+    this.sistemaService.getAllSistemas().subscribe((data) => {
+      this.sistemasList = data.response;
+    })
+  }
+
+  getAllUsuarios() {
+    this.usuarioService.getUsuarios().subscribe((data) => {
+      this.usuariosList = data.response
+    })
+  }
+
+  onSubmit(): void{
+    if (this.form.valid) {
+      const { id, usuario, sistema } = this.form.getRawValue();
+
+      const request: UsuarioSistemaInsertRequest = {
+        Sistema_Id: sistema,
+        Usuario_Id: usuario,
+        Estatus: 1
+      }
+
+      const requestUpdate: UsuarioSistemaUpdateRequest = {
+        Id: id,
+        Sistema_Id: sistema,
+        Usuario_Id: usuario,
+        Estatus: 1
+      }
+
+      const serviceCall = id === 0 ? this.usuarioSistemaService.insertUsuarioSistema(request) : this.usuarioSistemaService.updateUsuarioSistema(requestUpdate)
+      serviceCall.subscribe({
+        next: (res: any) => {
+          this.resetForm();
+          this.getAllUsuariosSistemas();
+        },
+        error: (err: any) => {
+          console.log(err)
+        }
+      });
+    } else {
+      this.form.markAllAsTouched
+    }
+    
+  }
+
+  resetForm() {
+    this.form.reset({
+      id: 0,
+      sistema: 0,
+      usuario: 0
     })
   }
 
